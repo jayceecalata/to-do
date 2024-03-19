fdebe25b5ea397f6f92fc546cffc8a0315d2a832
jeiz <jayceecalata@gmail.com>
2024-03-19 14:28:51 +0800

    feat: Create a class for 'todo-item'

---
+++ src/logic/item.js
@@ -0,0 +1,8 @@
+class Item {
+    constructor(title, description, due, priority) {
+        this.title = title;
+        this.description = description;
+        this.due = due;
+        this.priority = priority;
+    }
+}
